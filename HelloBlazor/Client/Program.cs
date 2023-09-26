16a95df8f0009331a62a750c153e99d34f7a370f
oleeriksen <oleeriksen1958@gmail.com>
2023-09-26 10:12:26 +0200

    localstorage

--- HelloBlazor/Client/Program.cs
+++ HelloBlazor/Client/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using HelloBlazor.Client;
+using Blazored.LocalStorage;
 
 namespace HelloBlazor.Client;
 
@@ -11,6 +12,8 @@ public class Program
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
         builder.RootComponents.Add<App>("#app");
         builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        builder.Services.AddBlazoredLocalStorage();
 
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
