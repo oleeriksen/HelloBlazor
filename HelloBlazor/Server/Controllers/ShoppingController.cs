e5aef557906e8465e5015cd9f6b2207bee72de86
oleeriksen <oe@easv.dk>
2023-10-05 13:46:44 +0200

    shopping

---
+++ HelloBlazor/Server/Controllers/ShoppingController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HelloBlazor.Shared;
+using HelloBlazor.Server.Repositories;
+
+namespace HelloBla.Server.Controllers
+{
+    [ApiController]
+    [Route("api/Shopping")]
+    public class ShoppingController : ControllerBase
+    {
+
+        private static ShoppingRepositoryInMemory mRepo = new();
+
+        [HttpGet]
+        [Route("getall")]
+        public IEnumerable<ShoppingItem> GetAll()
+        {
+            Console.WriteLine("GetAll ");
+            //Thread.Sleep(3000);
+            return mRepo.GetAll();
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public void AddItem(ShoppingItem product)
+        {
+            Console.WriteLine("Post " + product.Name);
+            mRepo.AddItem(product);
+         
+        }
+
+
+    }
+}
+
---
+++ HelloBlazor/Server/Repositories/ShoppingRepositoryInMemory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using HelloBlazor.Shared;
+
+namespace HelloBlazor.Server.Repositories
+{
+    public class ShoppingRepositoryInMemory
+    {
+        private List<ShoppingItem> mProducts = new() {
+                  new ShoppingItem { Name = "bananer", Id = 1, Price = 12, Done = true },
+                  new ShoppingItem { Name = "Æbler", Id = 2, Price = 14, Done = false  }
+        };
+        
+        public void AddItem(ShoppingItem item)
+        {
+            mProducts.Add(item);
+        }
+
+        public ShoppingItem[] GetAll()
+        {
+            return mProducts.ToArray();
+        }
+    }
+}
+
