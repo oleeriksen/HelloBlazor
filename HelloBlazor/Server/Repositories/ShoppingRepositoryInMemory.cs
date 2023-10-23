0500a825fff666246fbe094d7779c81f3b32d964
oleeriksen <oe@easv.dk>
2023-10-23 11:30:43 +0200

    Update ShoppingRepositoryInMemory.cs

--- HelloBlazor/Server/Repositories/ShoppingRepositoryInMemory.cs
+++ HelloBlazor/Server/Repositories/ShoppingRepositoryInMemory.cs
@@ -6,24 +6,27 @@ namespace HelloBlazor.Server.Repositories
     public class ShoppingRepositoryInMemory : IShoppingRepository
     {
         private List<ShoppingItem> mProducts = new() {
-                  new ShoppingItem { Name = "bananer", Price = 12, Done = true },
-                  new ShoppingItem { Name = "Æbler", Price = 14, Done = false  }
+                  new ShoppingItem { Id = 1, Name = "bananer", Price = 12, Done = true },
+                  new ShoppingItem { Id = 2, Name = "Æbler", Price = 14, Done = false  }
         };
 
         public void AddItem(ShoppingItem item){
+            int newId = mProducts.Select(item => item.Id).Max() + 1;
+            item.Id = newId;
             mProducts.Add(item);
         }
 
         public void DeleteById(int id)
         {
-            throw new NotImplementedException();
+            mProducts.RemoveAll(item => item.Id == id);
         }
 
         public ShoppingItem[] GetAll() => mProducts.ToArray();
 
         public void UpdateItem(ShoppingItem item)
         {
-            throw new NotImplementedException();
+            DeleteById(item.Id);
+            mProducts.Add(item);
         }
     }
 }
